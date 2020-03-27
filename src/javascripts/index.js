c3186a5e56454009e0a33b0f5612aa3fe4881de2
London <london@Londons-MacBook-Pro.local>
2020-03-27 11:25:54 -0600

    Final submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,50 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+import "bootstrap"
+
+let f = document.getElementById("myForm")
+f.onsubmit = function(){
+    let n = document.getElementById("name").value
+    alert("Welcome " + n)
+}
+
+const displayRandomBox = function(){
+    let r = Math.random()
+    if(r < .5){
+        document.write('<div class="red box"></div>')
+    }
+    else{
+        document.write('<div class="blue box"></div>')
+    }
+}
+
+function displayNBoxesUsingFor(n){
+    for(let i = 0; i < n; i++){
+        displayRandomBox()
+     }
+}
+
+function displayNBoxesUsingWhile(n){
+    let j = 0;
+    while(j < n){
+        displayRandomBox()
+    j++
+    }
+}
+
+function displayNBoxesUsingDoWhile(n){
+    let k = 0
+    do{
+        displayRandomBox()
+        k++
+    }while(k < n)
+}
+
+displayNBoxesUsingFor(10)
+displayNBoxesUsingWhile(10)
+displayNBoxesUsingDoWhile(10)
+
+const square = function(x){
+    return x * x
+}
